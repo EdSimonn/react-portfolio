680e50e7e09d01b4770d1dc5dac14a87f3108065
EdSimonn <dami25edusi@gmail.com>
2022-01-15 03:42:35 -0800

    First commit

---
+++ src/components/about/About.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './about.css'
+import Award from '../../img/award.png'
+
+export const About = () => {
+    return (
+        <div className="a">
+            <div className="a-left">
+                <div className="a-card bg"></div>
+                <div className="a-card">
+                    <img 
+                        src="https://i.ibb.co/7GkqGP4/thomas-le-HLf8-Nd-Gc-I0g-unsplash.jpg" 
+                        alt="" 
+                        className="a-img" 
+                    />
+                </div>
+            </div>
+            <div className="a-right">
+                <h1 className="a-title">About Me</h1>
+                <p className="a-sub">
+                    Lorem ipsum and Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat duis aute irure dolor in reprehende. 
+                </p>
+                <p className="a-desc">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+                </p>
+                <div className="a-award">
+                    <img src={Award} alt="" className="a-award-img" />
+                    <div className="a-award-texts">
+                        <h4 className="a-award-title">International Design Awards 2021</h4>
+                        <p className="a-award-desc">
+                            <p className="words">
+                                 Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit
+                                and fugit
+                            </p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/dg13cJ4/screencapture-localhost-3001-2022-01-14-07-10-12.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/9TGK9Kh/screencapture-localhost-3001-2022-01-14-07-15-10.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/dg13cJ4/screencapture-localhost-3001-2022-01-14-07-10-12.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://i.ibb.co/9TGK9Kh/screencapture-localhost-3001-2022-01-14-07-15-10.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://i.ibb.co/dg13cJ4/screencapture-localhost-3001-2022-01-14-07-10-12.png",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://i.ibb.co/9TGK9Kh/screencapture-localhost-3001-2022-01-14-07-15-10.png",
+      link: "http://lama.dev",
+    },
+];
+  
